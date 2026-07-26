12d70eb4b5a13aeaa7c0545ba417d61e3dd7f082
mounika22-ui <mounikasista95@gmail.com>
2026-07-26 21:55:23 +0530

    studenttable

--- src/components/studenttable.jsx
+++ src/components/studenttable.jsx
@@ -1,9 +1,11 @@
-function StudentTable({ students }) {
+import { Link } from "react-router-dom";
+
+function StudentTable({ students, deleteStudent }) {
   return (
     <>
-      {students.length === 0 ? 
+      {students.length === 0 ? (
         <h3>No Student Registered</h3>
-       : 
+      ) : (
         <table border="1" cellPadding="10">
           <thead>
             <tr>
@@ -13,23 +15,32 @@ function StudentTable({ students }) {
               <th>Phone</th>
               <th>Branch</th>
               <th>CGPA</th>
+              <th>Actions</th>
             </tr>
           </thead>
 
           <tbody>
             {students.map((student, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{student.StudentName}</td>
-                <td>{student.Email}</td>
-                <td>{student.Phone}</td>
-                <td>{student.Branch}</td>
-                <td>{student.Cgpa}</td>
+              <tr key={student._id}>
+                <td>{student._id+ 1}</td>
+                <td>{student.studentName}</td>
+                <td>{student.email}</td>
+                <td>{student.phone}</td>
+                <td>{student.branch}</td>
+                <td>{student.cgpa}</td>                                                                                                             
+
+                <td>
+                  <Link to={`/students/${student._id}`}>View</Link>{" "}
+                  <button onClick={() => deleteStudent(student._id)}>
+                    Delete
+                  </button>{" "}
+                  <Link to={`/students/edit/${student._id}`}>Edit</Link>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
-      }
+      )}
     </>
   );
 }
