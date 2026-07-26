687e717f6ddb8877a9740788bc5954750b2008ac
mounika22-ui <mounikasista95@gmail.com>
2026-07-26 21:54:50 +0530

    company deatils

--- src/pages/Companies/CompanyDetails.jsx
+++ src/pages/Companies/CompanyDetails.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from "../../api/api";
 
 function CompanyDetails() {
-
   const { id } = useParams();
+  const [company, setCompany] = useState(null);
+
+  useEffect(() => {
+    async function fetchCompany() {
+      try {
+        const token = localStorage.getItem("token");
+
+        const response = await api.get(`/companies/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        setCompany(response.data.company);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchCompany();
+  }, [id]);
+
+  if (!company) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Company Details</h1>
-      <h2>Company ID: {id}</h2>
+
+      <p><strong>Name:</strong> {company.name}</p>
+      <p><strong>Location:</strong> {company.location}</p>
+      <p><strong>Employees:</strong> {company.num_of_emp}</p>
     </div>
   );
 }
